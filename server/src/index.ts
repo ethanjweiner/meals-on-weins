20686d17b3305d7941c55efb2c48956eba13c9ba
Ethan Weiner <ethanweiner20@gmail.com>
2023-06-28 17:26:40 -0400

    refactor: move schema to separate gql file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,18 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './schema.ts';
+import { readFileSync } from 'fs';
+import { addMocksToSchema } from '@graphql-tools/mock';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import mocks from './mocks.ts';
 
 async function startServer() {
+  const typeDefs = readFileSync('./src/schema.graphql', { encoding: 'utf-8' });
+
   const server = new ApolloServer({
-    typeDefs,
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks,
+    }),
   });
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
---
+++ server/src/mocks.ts
@@ -0,0 +1,13 @@
+// const mocks = {
+//   Meal: {
+//     id: '9bf2f097-59c9-4a00-a38a-17c6905e6b7b',
+//     name: 'Monster Mash',
+//     imageSrc:
+//       'https://ifoodreal.com/wp-content/uploads/2019/08/fg-instant-pot-ground-turkey-quinoa-bowls.jpg',
+//     maxServings:
+//   },
+// };
+
+const mocks = {};
+
+export default mocks;
