38c73164b6326664eedf522fb240099af264020b
Ethan Weiner <ethanweiner20@gmail.com>
2023-06-28 16:36:21 -0400

    feat: initialize backend

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema.ts';
+
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs,
+  });
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  console.log(`🚀  Server ready at: ${url}`);
+}
+
+startServer();
